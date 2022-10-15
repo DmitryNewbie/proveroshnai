9c12dc42fb320931d3819ef52edfe3409bc69d09
Dmitry <zimzulka1@gmail.com>
2022-10-15 22:43:36 +0400

    raboshai versia

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+
+string[] array= {"111","2222","333","44","5" };
+
+var result = new string [array.Length];
+int size = 0;
+foreach(var count in array)
+{
+    if(count.Length <=3)
+    {
+        result[size] = count;
+        size++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine,result,0,size));
