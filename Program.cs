aaade10fb6744a0b70ffb2657649b7ebb9252b06
Dmitry <zimzulka1@gmail.com>
2022-10-16 00:13:39 +0400

    probnaia

--- Program.cs
+++ Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 
-string[] array= {"111","2222","333","44","5" };
+//string[] array= {"111","2222","333","44","5" };
 
-var result = new string [array.Length];
-int size = 0;
-foreach(var count in array)
+//var result = new string [array.Length];
+//int size = 0;
+//foreach(var count in array)
+//{
+//    if(count.Length <=3)
+//    {
+//        result[size] = count;
+//        size++;
+//    }
+//}
+//Console.WriteLine(string.Join(Environment.NewLine,result,0,size));
+
+Console.WriteLine("Введите цифры");
+int [] array = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+
+var result = new int [array.Length];
+
+for(int i=0; i<array.Length; i++)
 {
-    if(count.Length <=3)
+    if(array[i]<=3)
     {
-        result[size] = count;
-        size++;
+        result[i] = array.Length;
+        i++;
     }
 }
-Console.WriteLine(string.Join(Environment.NewLine,result,0,size));
+Console.WriteLine($"{result} ");
+
